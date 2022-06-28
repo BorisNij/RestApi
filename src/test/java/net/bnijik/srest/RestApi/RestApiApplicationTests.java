0e918f7538866e4f3ece104a8ff5005be951b862
Boris N <borisnijikovsky@gmail.com>
2022-06-27 21:57:11 -0400

    Add spinning up of postgres testcontainer

--- src/test/java/net/bnijik/srest/RestApi/RestApiApplicationTests.java
+++ src/test/java/net/bnijik/srest/RestApi/RestApiApplicationTests.java
@@ -1,10 +1,26 @@
 package net.bnijik.srest.RestApi;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+
+@Testcontainers
 class RestApiApplicationTests {
+
+    @SuppressWarnings("resource")
+    @Container
+    private static final JdbcDatabaseContainer<?> postgresDb = new PostgreSQLContainer<>("postgres:latest")
+            .withInitScript("init.sql");
+
+    /*@DynamicPropertySource
+    public static void overrideProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresDb::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresDb::getUsername);
+        registry.add("spring.datasource.password", postgresDb::getPassword);
+    }*/
 
     @Test
     void contextLoads() {
